145d7a7a4555af42720bf5bec23a80670d096aa6
sukiboo <dereventsov@gmail.com>
2021-03-22 18:38:22 -0400

    fix tickers

--- nootifications.py
+++ nootifications.py
@@ -13,8 +13,8 @@ import json
 class CryptoNoot:
 
     def __init__(self):
-        self.assets = ['ETH/USD', 'BTC/USD']
-        self.margin = .005
+        self.margin = .01
+        self.tickers = {'ETH/USD': 'eth', 'XBT/USD': 'btc'}
         self.contact = 'REDACTED_CHAT_ID'
         self.bot_token = 'REDACTED_TOKEN'
         self.price_log = './prices.log'
@@ -23,20 +23,21 @@ class CryptoNoot:
     def initialize_prices(self):
         try:
             self.log_price = json.load(open(self.price_log, 'r'))
-            assert all(ticker in self.log_price for ticker in self.assets)
+            assert all(ticker in self.log_price for ticker in self.tickers)
         except:
             self.log_price = {}
             url = 'https://api.kraken.com/0/public/Ticker?pair='
-            for ticker in self.assets:
-                price = requests.get(url + ticker).json()['result'][ticker]['c'][0]
-                self.log_price[ticker] = float(price)
+            for ticker in self.tickers:
+                asset_pair = self.tickers[ticker].upper() + '/USD'
+                result = requests.get(url + asset_pair).json()['result']
+                self.log_price[ticker] = float(result[asset_pair]['c'][0])
             json.dump(self.log_price, open(self.price_log, 'w+'))
 
     def start_websocket(self):
         client = kraken_wsclient_py.WssClient()
         client.start()
         client.subscribe_public(subscription={'name':'ticker'},
-            pair=self.assets, callback=self.process_event)
+            pair=list(self.tickers), callback=self.process_event)
 
     def process_event(self, event):
         try:
@@ -51,7 +52,8 @@ class CryptoNoot:
         if change > self.margin:
             direction = 'up' if price > self.log_price[ticker] else 'down'
             self.update_price(ticker, price)
-            self.send_nootification(f'{ticker} is {direction} {100*change:.2f}% to {price:.2f}')
+            self.send_nootification(f'{self.tickers[ticker]}'\
+                + f' is {direction} {100*change:.2f}% to {price:.2f}')
 
     def update_price(self, ticker, price):
         self.log_price[ticker] = price
